7cac44e551e7d2e6e05195a9531a6bbe836df051
Tom Johnson <tarjohnson1979@gmail.com>
2018-12-25 20:52:13 +0000

    Add a Timer to the orrery

--- src/Examples/Orrery/Orrery.hs
+++ src/Examples/Orrery/Orrery.hs
@@ -1,17 +1,24 @@
 module Examples.Orrery.Orrery where
 
 import Graphics.Gloss
+import Control.Lens
 
 import ReduxGame.Redux
+import ReduxGame.Timer
 import ReduxGame.Renderer.Renderable
 
 data Orrery = Orrery
+  { _timer :: Timer
+  }
+
+makeLenses ''Orrery
 
 orrery :: Orrery
-orrery = Orrery
+orrery = Orrery newTimer
 
 instance Renderable Orrery where
   render _ = color yellow $ circleSolid 50
 
 orreryRedux :: Redux Orrery
 orreryRedux = redux
+          |+> connect timer timerRedux
