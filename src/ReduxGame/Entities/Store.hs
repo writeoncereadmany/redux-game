90455c49ae7e281a8c64d130425814c9db9fa0b0
Tom Johnson <tarjohnson1979@gmail.com>
2019-01-04 09:01:52 +0000

    Maintain ordering in the store

--- src/ReduxGame/Entities/Store.hs
+++ src/ReduxGame/Entities/Store.hs
@@ -10,14 +10,16 @@ withId :: EntityId -> Store a -> Maybe a
 withId entId (Store xs) = withId' xs where
   withId' [] = Nothing
   withId' ((Tagged entId' a) : as) =
-    if entId == entId'
-      then Just a
-      else withId' as
+    case compare entId entId' of
+      LT -> withId' as
+      EQ -> Just a
+      GT -> Nothing
 
 replaceComponent :: EntityId -> a -> Store a -> Store a
 replaceComponent entId a (Store xs) = Store $ replaceComponent' xs where
   replaceComponent' [] = [ Tagged entId a ]
-  replaceComponent' (c@(Tagged entId' _) : cs) =
-    if entId == entId'
-      then (Tagged entId a) : cs
-      else c : replaceComponent' cs
+  replaceComponent' cs@(c@(Tagged entId' _) : restc) =
+    case compare entId entId' of
+      LT -> (Tagged entId a) : cs
+      EQ -> (Tagged entId a) : restc
+      GT -> c : replaceComponent' restc
