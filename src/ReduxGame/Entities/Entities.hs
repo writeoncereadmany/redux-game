bb944d57ae325f744e839632a5a38d0d8a0f6c5a
Tom Johnson <tarjohnson1979@gmail.com>
2019-01-04 08:54:49 +0000

    Parallel iterative apply2. Relies on the entities in the store being ordered, which isn't currently true

--- src/ReduxGame/Entities/Entities.hs
+++ src/ReduxGame/Entities/Entities.hs
@@ -43,3 +43,26 @@ setComponent component entityId = Entities $ \components ->
       store' = replaceComponent entityId component store
       components' = replaceStore store' components
    in ((), components')
+
+apply2 :: forall a b . (Component a, Component b)
+       => ((a, b) -> (a, b))
+       -> ComponentStore
+       -> ComponentStore
+apply2 f components = let
+     as = storeOf components
+     bs = storeOf components
+     (as', bs') = apply2'' as bs
+  in replaceStore as' $ replaceStore bs' $ components where
+    apply2'' :: Store a -> Store b -> (Store a, Store b)
+    apply2'' (Store as) (Store bs) = let (as', bs') = apply2' as bs in (Store as', Store bs')
+    apply2' :: [ Tagged a ] -> [ Tagged b] -> ([ Tagged a ], [ Tagged b ])
+    apply2' [] bs = ([], bs)
+    apply2' as [] = (as, [])
+    apply2' as@(firsta@(Tagged id_a a):resta) bs@(firstb@(Tagged id_b b):restb) = case compare id_a id_b of
+      LT -> let (as', bs') = apply2' resta bs
+             in (firsta:as', bs')
+      GT -> let (as', bs') = apply2' as restb
+             in (as', firstb:bs')
+      EQ -> let (a', b') = f (a, b)
+                (as', bs') = apply2' resta restb
+             in ((Tagged id_a a'):as', (Tagged id_b b'):bs')
