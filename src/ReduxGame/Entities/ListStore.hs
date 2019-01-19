0674fcc062c4971a6dba6871d541ad7c877e4974
Tom Johnson <tarjohnson1979@gmail.com>
2019-01-19 15:19:38 +0000

    Allow stores to expose contents as lists, to be iterated over. Maybe we can offer combined views more effectively for sparse vectors, but we'll cross that bridge when we come to it

--- src/ReduxGame/Entities/ListStore.hs
+++ src/ReduxGame/Entities/ListStore.hs
@@ -16,6 +16,8 @@ liststore_emptyStore = ListStore []
 instance Store ListStore where
   withId = liststore_withId
   replaceComponent = liststore_replaceComponent
+  components (ListStore a) = a
+  mergeComponents = liststore_mergeComponents
   apply2 = liststore_apply2
   emptyStore = liststore_emptyStore
   delete = liststore_delete
@@ -30,13 +32,29 @@ liststore_withId entId (ListStore xs) = withId' xs where
       GT -> withId' as
 
 liststore_replaceComponent :: EntityId -> a -> ListStore a -> ListStore a
-liststore_replaceComponent entId a (ListStore xs) = ListStore $ replaceComponent' xs where
-  replaceComponent' [] = [ Tagged entId a ]
-  replaceComponent' cs@(c@(Tagged entId' _) : restc) =
-    case compare entId entId' of
-      LT -> (Tagged entId a) : cs
-      EQ -> (Tagged entId a) : restc
-      GT -> c : replaceComponent' restc
+liststore_replaceComponent entId a = mergeComponents [Tagged entId a]
+
+liststore_mergeComponents :: [ Tagged a ] -> ListStore a -> ListStore a
+liststore_mergeComponents new (ListStore old) = ListStore $ mergeComponents' new old where
+  mergeComponents' :: [Tagged a] -> [Tagged a] -> [Tagged a]
+  mergeComponents' new [] = new
+  mergeComponents' [] old = old
+  mergeComponents' news@(new@(Tagged new_id new_c) : rest_news) olds@(old@(Tagged old_id old_c) : rest_olds) =
+    case compare new_id old_id of
+      GT -> old : mergeComponents' news rest_olds
+      EQ -> new : mergeComponents' rest_news rest_olds
+      LT -> new : mergeComponents' rest_news olds
+
+
+liststore_getPairs :: ListStore a -> ListStore b -> [ Tagged (a, b) ]
+liststore_getPairs (ListStore as) (ListStore bs) = getPairs as bs where
+  getPairs :: [Tagged a] -> [Tagged b] -> [Tagged (a, b)]
+  getPairs [] _ = []
+  getPairs _ [] = []
+  getPairs all_as@((Tagged id_a a): rest_a) all_bs@((Tagged id_b b): rest_b) = case compare id_a id_b of
+    EQ -> Tagged id_a (a, b) : getPairs rest_a rest_b
+    LT -> getPairs rest_a all_bs
+    GT -> getPairs all_as rest_b
 
 liststore_apply2 :: forall a b . ((a, b) -> (a, b)) -> ListStore a -> ListStore b -> (ListStore a, ListStore b)
 liststore_apply2 f (ListStore as) (ListStore bs) = let (as', bs') = apply2' as bs in (ListStore as', ListStore bs') where
--- src/ReduxGame/Entities/Store.hs
+++ src/ReduxGame/Entities/Store.hs
@@ -6,9 +6,30 @@ type EntityId = Integer
 
 data Tagged a = Tagged EntityId a
 
+instance Functor Tagged where
+  fmap f (Tagged entId a) = Tagged entId (f a)
+
 class Typeable t => Store t where
   withId :: EntityId -> t a -> Maybe a
   replaceComponent :: EntityId -> a -> t a -> t a
+  mergeComponents :: [ Tagged a ] -> t a -> t a
+  components :: t a -> [ Tagged a ]
   apply2 :: ((a, b) -> (a, b)) -> t a -> t b -> (t a, t b)
   emptyStore :: t a
   delete :: EntityId -> t a -> t a
+
+combine2 :: [ Tagged a ] -> [ Tagged b ] -> [ Tagged (a, b) ]
+combine2 _ [] = []
+combine2 [] _ = []
+combine2 all_as@(Tagged a_id a : rest_as) all_bs@(Tagged b_id b : rest_bs)
+  | a_id == b_id = Tagged a_id (a, b) : combine2 rest_as rest_bs
+  | a_id > b_id = combine2 all_as rest_bs
+  | a_id < b_id = combine2 rest_as all_bs
+
+combine3 :: [ Tagged a ] -> [ Tagged b ] -> [Tagged c ] -> [ Tagged (a, b, c)]
+combine3 as bs cs = (flatten <$>) <$> (combine2 as (combine2 bs cs)) where
+  flatten (a, (b, c)) = (a, b, c)
+
+combine4 :: [ Tagged a ] -> [ Tagged b ] -> [Tagged c ] -> [ Tagged d ] -> [ Tagged (a, b, c, d)]
+combine4 as bs cs ds = (flatten <$>) <$> (combine2 (combine2 as bs) (combine2 cs ds)) where
+  flatten ((a, b), (c, d)) = (a, b, c, d)
