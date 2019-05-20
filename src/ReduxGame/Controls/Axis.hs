857a88c0b1595be7a8ac74f2ea06ace6dd0547b1
Tom Johnson <tarjohnson1979@gmail.com>
2019-05-20 10:27:37 +0100

    Add code for button and axis controls

---
+++ src/ReduxGame/Controls/Axis.hs
@@ -0,0 +1,32 @@
+module ReduxGame.Controls.Axis where
+
+import Control.Lens
+import Graphics.Gloss.Interface.IO.Game
+
+import ReduxGame.Redux
+import ReduxGame.Controls.Button
+
+data OnAxis = Min | Neutral | Max
+
+data Axis = Axis
+  { _minButton :: Button
+  , _maxButton :: Button
+  , _onAxis    :: OnAxis
+  }
+
+makeLenses ''Axis
+
+axis :: Button -> Button -> Axis
+axis min max = Axis min max Neutral
+
+updateAxis :: Axis -> Axis
+updateAxis axis = case (held $ axis ^. minButton, held $ axis ^. maxButton) of
+  (True, False) -> onAxis .~ Min     $ axis
+  (False, True) -> onAxis .~ Max     $ axis
+  (_, _)        -> onAxis .~ Neutral $ axis
+
+axisPress :: Event -> Axis -> Events Axis
+axisPress event axis = return axis
+                   >>= minButton %%~ keyPress event
+                   >>= maxButton %%~ keyPress event
+                   <&> updateAxis
---
+++ src/ReduxGame/Controls/Button.hs
@@ -0,0 +1,27 @@
+module ReduxGame.Controls.Button where
+
+import Graphics.Gloss.Interface.IO.Game
+
+import ReduxGame.Redux
+
+-- no lenses, because of Events () fields: ghc doesn't support impredicative polymorphism
+data Button = Button
+  { boundKey  :: Key
+  , held      :: Bool
+  , onPress   :: Events ()
+  , onRelease :: Events ()
+  }
+
+button :: Char -> Button
+button key = Button { boundKey = Char key, held = False, onPress = return (), onRelease = return () }
+
+keyPress :: Event -> Button -> Events Button
+keyPress (EventKey key pressed _ _) button =
+    if key /= (boundKey button)
+  then return button
+  else case pressed of
+     Down -> do onPress button
+                return (button { held = True})
+     Up   -> do onRelease button
+                return (button { held = False })
+keyPress _ button = return button
