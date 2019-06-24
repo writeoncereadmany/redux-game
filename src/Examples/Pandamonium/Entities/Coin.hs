a7ba60682255358c4f8024deb20cfed160d96bef
Tom Johnson <tarjohnson1979@gmail.com>
2019-06-24 23:28:06 +0100

    Extract the fundamental operations on a redux into a separate file, as step 1 of moving towards a typeclass with multiple intentions (like a dict-backed redux)

--- src/Examples/Pandamonium/Entities/Coin.hs
+++ src/Examples/Pandamonium/Entities/Coin.hs
@@ -21,7 +21,7 @@ frames image = cycle [ scale 4 4 (BitmapSection r image) | r <- sprites ]
 coin :: PandaAssets -> Vector -> Entity
 coin assets position = entity
             <-+ Coin
-            <-+ circle (0, 0) 0116
+            <-+ circle (0, 0) 32
             <-+ Position position
             <-+ head (frames $ assets ^. coin_sprites)
             <-+ AnimationFrames (tail (frames $ assets ^. coin_sprites))
--- src/ReduxGame/Redux.hs
+++ src/ReduxGame/Redux.hs
@@ -15,7 +15,7 @@ module ReduxGame.Redux
   , (|=>)
   , (|->)
   , (|::)
-  )where
+  ) where
 
 import Data.Dynamic (Typeable)
 import Data.ConstrainedDynamic
@@ -23,43 +23,16 @@ import Graphics.Gloss.Interface.IO.Game
 import Control.Monad.Writer
 import Control.Lens
 import Data.DList
-class (Typeable a) => ReduxEvent a
 
-type DynEvent = ConstrainedDynamic ReduxEvent
+import ReduxGame.ReduxImpl
 
-instance ReduxEvent Event
 data BeforeTimeStep = BeforeTimeStep deriving ReduxEvent
 data TimeStep = TimeStep Float deriving ReduxEvent
 
-type Events w = WriterT (DList DynEvent) IO w
-type Updater a b = forall l . (Functor l, Applicative l) => LensLike' l b a
-
-type Redux w = DynEvent -> w -> Events w
-
-focus :: (ReduxEvent a) => (a -> b -> Events b) -> Redux b
-focus f e w = case (fromDynamic e) of
-  Just x -> f x w
-  Nothing -> return w
-
-pureFocus :: (ReduxEvent a) => (a -> b -> b) -> DynEvent -> b -> b
-pureFocus f e w = case (fromDynamic e) of
-  Just x -> f x w
-  Nothing -> w
+type Redux w = ReduxFun w
 
 fireEvent :: ReduxEvent a => a -> Events ()
 fireEvent = tell . singleton . toDyn
-
-handleRemainingEvents :: Redux w -> w -> DList DynEvent -> IO w
-handleRemainingEvents f world events = do
-  (world', events') <- runWriterT $ foldM (flip f) world events
-  case events' of
-    Nil -> return world'
-    otherwise -> handleRemainingEvents f world' events'
-
-reduxDo :: Redux w -> w -> Events () -> IO w
-reduxDo r w a = do
-  ((), events) <- runWriterT a
-  handleRemainingEvents r w events
 
 reduxUpdate :: Redux w -> Float -> w -> IO w
 reduxUpdate f timestep world = reduxDo f world $ do
@@ -75,33 +48,19 @@ onButton expected action (EventKey (Char actual) _ _ _) a = do
   return a
 onButton _ _ _ a = return a
 
-connect :: Updater a b -> (i -> a -> Events a) -> (i -> b -> Events b)
-connect lens f e = lens %%~ (f e)
+infixl 1 |::
+(|::) :: Redux w -> Redux w -> Redux w
+(|::) = reduxCons
 
-redux :: Redux a
-redux = const return
+infixl 1 |=>
+(|=>) :: ReduxEvent a => Redux w -> (a -> w -> Events w) -> Redux w
+(|=>) = reduxBind
 
 infixl 1 |!>
-
 (|!>) :: ReduxEvent a => Redux w -> (a -> Events ()) -> Redux w
 (|!>) redux f = redux |=> doing f where
   doing action a b = do action a; return b
 
-infixl 1 |=>
-
-(|=>) :: ReduxEvent a => Redux w -> (a -> w -> Events w) -> Redux w
-(|=>) redux f e w = return w
-                >>= redux e
-                >>= focus f e
-
 infixl 1 |->
-
 (|->) :: ReduxEvent a => Redux w -> (a -> w -> w) -> Redux w
 (|->) redux f e w = (|=>) redux (\a w -> return $ f a w) e w
-
-
-infixl 1 |::
-(|::) :: Redux w -> Redux w -> Redux w
-(|::) redux1 redux2 e w = return w
-                      >>= redux1 e
-                      >>= redux2 e
